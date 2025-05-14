ee6835eb1be604ac5db9d82a430017dbcb320fb4
Proust <sultanmkhalid123@gmail.com>
2025-05-14 15:27:54 +0500

    Initial Commit

---
+++ New folder/script.js
@@ -0,0 +1,10 @@
+// let min = 1, max = 10
+
+// function randy(min, max) {
+//     let random = Math.floor(Math.random() * (max - min + 1) + min)
+
+//     console.log(random);
+    
+// }
+
+// randy(min, max)
